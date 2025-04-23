3c977bb6b14f29e7807b27870782db9965483325
ludwig <ludwig@frostudios.com>
2025-04-23 09:33:59 +0200

    added more routes

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,6 +1,14 @@
 from routes.api.users import api as users_api
 from routes.web.home import web as web_routes
+from routes.api.companies import api as companies_api
+from routes.web.register import register as register_routes
+from routes.web.login import login as login_routes
+from routes.web.dashboard import dashboard as dashboard_routes
 
-def register_routes(app):
+def init_routes(app):  # Renamed function to avoid conflict
     app.register_blueprint(users_api)
     app.register_blueprint(web_routes)
+    app.register_blueprint(register_routes)
+    app.register_blueprint(companies_api)
+    app.register_blueprint(login_routes)
+    app.register_blueprint(dashboard_routes)
