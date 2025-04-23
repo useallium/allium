70f348e0fc8fca515afe83d26f479e4e0b76ed15
ludwig <ludwig@frostudios.com>
2025-04-23 09:32:37 +0200

    fixed some mistyping and added sessions support

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask
-from routes import register_routes
+from routes import init_routes
+import secrets
+
 
 def create_app():
     app = Flask(__name__)
+    app.secret_key = secrets.token_hex(32)
+    app.config['SESSION_TYPE'] = 'filesystem'  # You can use 'redis', 'memcached', etc. too.
+    app.config['SESSION_PERMANENT'] = False    # Session should not be permanent by default
     app.config.from_pyfile('config.py')
 
-    register_routes(app)
+    init_routes(app)
     return app
 
 if __name__ == '__main__':
