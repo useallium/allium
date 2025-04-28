c969606d94b49cc74483f357937081187b9507a9
ludwig <ludwig@frostudios.com>
2025-04-28 21:22:56 +0200

    added file for database handling of users and added functions in the class who handles database communication

--- database/users.py
+++ database/users.py
@@ -2,12 +2,18 @@ from database.base import Database
 
 class Users(Database):
     def get_all_users(self):
+        """
+        Retrieve all users in database
+        """
         query = "SELECT * FROM Users"
         self.cursor.execute(query)
         users = self.cursor.fetchall()
         return users
     
     def get_user_by_id(self, user_id):
+        """
+        Retrieve specific user given their user_id
+        """
         query = "SELECT * FROM Users WHERE user_id = %s"
         self.cursor.execute(query, (user_id,))
         user = self.cursor.fetchone()
