25e393d291e3f7afb0d1ce7e0d5238fd55123d86
ludwig <ludwig@frostudios.com>
2025-04-28 11:54:06 +0200

    added file for testing

--- test.py
+++ test.py
@@ -1,12 +1,4 @@
 from db import connect
 
-def test_db():
-    conn = connect()
-    cursor = conn.cursor(dictionary=True)
-
-    cursor.execute("SELECT * FROM Users")
-    user = cursor.fetchone()
-
-    print(user)
-
-test_db()
+def test():
+    print("test")
