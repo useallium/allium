cf7b093e458496d0bde10ff9e0438aebd515a173
ludwig <ludwig@frostudios.com>
2025-05-12 11:25:48 +0300

    some functionality to student db

--- database/students.py
+++ database/students.py
@@ -7,5 +7,9 @@ class Students(Database):
         users = self.cursor.fetchall()
         return users
     
-
+    def get_student_by_id(self,student_id):
+        query = "SELECT * FROM Students WHERE student_id = %s"
+        self.cursor.execute(query,(student_id))
+        user = self.cursor.fetchone()
+        return user
     
