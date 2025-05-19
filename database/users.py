bb8571264ecd88033c67d67eae4980d43444a381
3xqwe <william-silvstrand@hotmail.com>
2025-05-19 13:07:56 +0200

    Removed update password functionality in update_user() in users.py

--- database/users.py
+++ database/users.py
@@ -20,7 +20,7 @@ class Users(Database):
         user = self.cursor.fetchone()
         return user
 
-    def update_user(self, user_id, email, first_name, last_name, password_hash, user_type, profile_picture):
+    def update_user(self, user_id, email, first_name, last_name, user_type, profile_picture):
         """
         Update a user's information in the database given their user_id.
         """
@@ -29,12 +29,11 @@ class Users(Database):
             SET email = %s,
                 first_name = %s,
                 last_name = %s,
-                password_hash = %s,
                 user_type = %s,
                 profile_picture = %s
             WHERE user_id = %s
         """
-        self.cursor.execute(query, (email, first_name, last_name, password_hash, user_type, profile_picture, user_id))
+        self.cursor.execute(query, (email, first_name, last_name, user_type, profile_picture, user_id))
         self.conn.commit()
         return self.cursor.rowcount
     
