8bac35b4049d5d633ae34a023db90012e20c9d38
ludwig <ludwig@frostudios.com>
2025-05-19 13:42:41 +0200

    added update students

--- database/students.py
+++ database/students.py
@@ -13,3 +13,18 @@ class Students(Database):
         user = self.cursor.fetchone()
         return user
     
+    def update_student(self, student_id, university, degree, graduation_year, resume):
+        """
+        Update a student's information in the database given their student_id.
+        """
+        query = """
+            UPDATE Students
+            SET university = %s,
+                degree = %s,
+                graduation_year = %s,
+                resume = %s
+            WHERE student_id = %s
+        """
+        self.cursor.execute(query, (university, degree, graduation_year, resume, student_id))
+        self.conn.commit()
+        return self.cursor.rowcount
